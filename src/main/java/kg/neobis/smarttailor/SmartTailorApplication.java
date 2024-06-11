3fa114c695a99faa0629f638671a7ae844f26ef4
imashevdt11 <imashevdt@gmail.com>
2024-06-11 17:22:35 +0600

    Add dependencies and configure application settings

---
+++ src/main/java/kg/neobis/smarttailor/SmartTailorApplication.java
@@ -0,0 +1,13 @@
+package kg.neobis.smarttailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTailorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTailorApplication.class, args);
+    }
+
+}
