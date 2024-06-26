49facd5332b0059fcfea3498b3a46a36b64acb87
Akzholbek Abdikarim uulu <akzholbek0096@gmail.com>
2024-06-26 23:30:44 +0100

    configure swagger

---
+++ src/main/java/kg/neobis/smarttailor/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package kg.neobis.smarttailor.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Smart Tailor",
+                        email = "smart-tailor@company.com"
+
+                ),
+                title = "Cooker corner mobile app",
+                description = "OpenApi documentation for Cooker Corner Spring Project",
+                version = "0.0.1"
+        ),
+        servers = {
+                @Server(
+                        description = "Server",
+                        url = "https://smarttailor.xyz"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
