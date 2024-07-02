25ac0dee6a659ba2b7d7970311a7fc00f1f5a0af
imashevdt11 <imashevdt@gmail.com>
2024-07-02 15:30:24 +0600

    moved some methods from EquipmentService to CloudinaryService and AppUserService

--- src/main/java/kg/neobis/smarttailor/service/impl/CloudinaryServiceImpl.java
+++ src/main/java/kg/neobis/smarttailor/service/impl/CloudinaryServiceImpl.java
@@ -2,6 +2,7 @@ package kg.neobis.smarttailor.service.impl;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import kg.neobis.smarttailor.entity.Image;
 import kg.neobis.smarttailor.service.CloudinaryService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -19,8 +22,30 @@ public class CloudinaryServiceImpl implements CloudinaryService {
     Cloudinary cloudinary;
 
     @Override
+    public void deleteImage(String imageUrl) throws IOException {
+        String publicId = extractPublicId(imageUrl);
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+
+    @Override
+    public String extractPublicId(String imageUrl) {
+        String withoutProtocol = imageUrl.replaceFirst("https?://[^/]+/", "");
+        String withoutPath = withoutProtocol.substring(0, withoutProtocol.lastIndexOf("/"));
+        return withoutPath.substring(withoutPath.lastIndexOf("/") + 1, withoutPath.lastIndexOf("."));
+    }
+
+    @Override
+    public List<Image> saveImages(List<MultipartFile> images) {
+        List<Image> imageList = new ArrayList<>();
+        for (MultipartFile file : images) {
+            imageList.add(new Image(uploadImage(file)));
+        }
+        return imageList;
+    }
+
+    @Override
     public String uploadImage(MultipartFile file){
-        Map uploadResult = null;
+        Map uploadResult;
         try {
             uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
         } catch (IOException e) {
@@ -28,14 +53,13 @@ public class CloudinaryServiceImpl implements CloudinaryService {
         }
         return (String) uploadResult.get("secure_url");
     }
+
     @Override
-    public void deleteProductImage(String imageUrl) throws IOException {
-        String publicId = extractPublicId(imageUrl);
-        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
-    }
-    public String extractPublicId(String imageUrl) {
-        String withoutProtocol = imageUrl.replaceFirst("https?://[^/]+/", "");
-        String withoutPath = withoutProtocol.substring(0, withoutProtocol.lastIndexOf("/"));
-        return withoutPath.substring(withoutPath.lastIndexOf("/") + 1, withoutPath.lastIndexOf("."));
+    public void validateImages(List<MultipartFile> images) {
+        for (MultipartFile image : images) {
+            if (image == null || image.isEmpty() || image.getContentType() == null || !image.getContentType().startsWith("image/")) {
+                throw new IllegalArgumentException("The file is not an image!");
+            }
+        }
     }
 }
--- src/main/java/kg/neobis/smarttailor/service/impl/EquipmentServiceImpl.java
+++ src/main/java/kg/neobis/smarttailor/service/impl/EquipmentServiceImpl.java
@@ -12,6 +12,7 @@ import kg.neobis.smarttailor.exception.InvalidJsonException;
 import kg.neobis.smarttailor.exception.ResourceNotFoundException;
 import kg.neobis.smarttailor.mapper.EquipmentMapper;
 import kg.neobis.smarttailor.repository.EquipmentRepository;
+import kg.neobis.smarttailor.service.AppUserService;
 import kg.neobis.smarttailor.service.CloudinaryService;
 import kg.neobis.smarttailor.service.EquipmentService;
 import lombok.AccessLevel;
@@ -25,8 +26,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -34,6 +33,7 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EquipmentServiceImpl implements EquipmentService {
 
+    AppUserService appUserService;
     EquipmentRepository equipmentRepository;
     EquipmentMapper equipmentMapper;
     ObjectMapper objectMapper;
@@ -48,7 +48,7 @@ public class EquipmentServiceImpl implements EquipmentService {
 
     @Override
     public EquipmentDto getEquipmentById(Long equipmentId) {
-        Equipment equipment = equipmentRepository.findById(equipmentId).orElseThrow(()-> new ResourceNotFoundException( "Equipment not found", HttpStatus.NOT_FOUND.value()));
+        Equipment equipment = equipmentRepository.findById(equipmentId).orElseThrow(() -> new ResourceNotFoundException("Equipment not found", HttpStatus.NOT_FOUND.value()));
         return equipmentMapper.entityToDto(equipment);
     }
 
@@ -56,48 +56,13 @@ public class EquipmentServiceImpl implements EquipmentService {
     public String addEquipment(String equipmentRequestDto, List<MultipartFile> images, Authentication authentication) {
 
         EquipmentRequestDto requestDto = parseAndValidateRecipeDto(equipmentRequestDto);
-        AppUser user = getUserFromAuthentication(authentication);
-        validateImages(images);
-        List<Image> equipmentImages = saveImages(images);
+        AppUser user = appUserService.getUserFromAuthentication(authentication);
+        cloudinaryService.validateImages(images);
+        List<Image> equipmentImages = cloudinaryService.saveImages(images);
 
         Equipment equipment = equipmentMapper.dtoToEntity(requestDto, equipmentImages, user);
         equipmentRepository.save(equipment);
         return "The equipment has been added successfully!";
-    }
-
-    private List<Image> saveImages(List<MultipartFile> images) {
-        List<Image> imageList = new ArrayList<>();
-        for(MultipartFile file: images){
-            try {
-                imageList.add(new Image(cloudinaryService.uploadImage(file)));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return imageList;
-    }
-
-    private void validateImages(List<MultipartFile> images) {
-        for (MultipartFile image : images) {
-            if (image == null || image.isEmpty() || image.getContentType() == null || !image.getContentType().startsWith("image/")) {
-                throw new IllegalArgumentException("The file is not an image!");
-            }
-        }
-    }
-
-
-
-
-    private AppUser getUserFromAuthentication(Authentication authentication) {
-        if (authentication != null && authentication.isAuthenticated()){
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof AppUser appUser){
-                return appUser;
-            }else {
-                throw new IllegalArgumentException("Principal is not an instance of AppUser");
-            }
-        }
-        return null;
     }
 
     private EquipmentRequestDto parseAndValidateRecipeDto(String equipmentDto) {
