08fed52bfdc0b5726ea3c593d62cacc62024872e
Akzholbek Abdikarim uulu <akzholbek0096@gmail.com>
2024-08-10 21:16:33 +0100

    test

--- src/main/java/kg/neobis/smarttailor/dtos/AdvertisementPageDto.java
+++ src/main/java/kg/neobis/smarttailor/dtos/AdvertisementPageDto.java
@@ -1,5 +1,6 @@
 package kg.neobis.smarttailor.dtos;
 
+import java.io.Serializable;
 import java.util.List;
 
-public record AdvertisementPageDto(List<?> advertisement, boolean isLast, Long totalCount) {}
+public record AdvertisementPageDto(List<?> advertisement, boolean isLast, Long totalCount) implements Serializable {}
