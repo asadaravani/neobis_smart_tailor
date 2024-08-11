177781a553820c041d8930ce86cd59485d51e4aa
Akzholbek Abdikarim uulu <akzholbek0096@gmail.com>
2024-08-11 20:59:23 +0100

    added websocket

---
+++ src/main/java/kg/neobis/smarttailor/config/NotificationWebSocketHandler.java
@@ -0,0 +1,38 @@
+package kg.neobis.smarttailor.config;
+
+import kg.neobis.smarttailor.dtos.NotificationDto;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class NotificationWebSocketHandler extends TextWebSocketHandler {
+
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    public void sendNotification(String message) throws Exception {
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(message));
+            }
+        }
+    }
+}
--- src/main/java/kg/neobis/smarttailor/service/NotificationService.java
+++ src/main/java/kg/neobis/smarttailor/service/NotificationService.java
@@ -1,10 +1,13 @@
 package kg.neobis.smarttailor.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import kg.neobis.smarttailor.dtos.NotificationDto;
 import kg.neobis.smarttailor.dtos.NotificationPdfDto;
 
 public interface NotificationService {
     void sendNotification(NotificationDto dto1, NotificationPdfDto dto2);
 
-    void sendNotification(NotificationDto dto1);
+    void sendNotification(NotificationDto dto1) throws JsonProcessingException;
+    void notifyUsers(String message);
+
 }
